6471971d50f9e2432f9816e226a77cc89bdd5f19
kshitiz-shresth <er.kshitizshrestha@gmail.com>
2022-07-05 00:51:56 +0545

    [prac] struct function more practice

--- structFunc.go
+++ structFunc.go
@@ -10,8 +10,24 @@ func (this *MyStruct) fnInsideStruct() {
 	fmt.Println("Hello Sir", this.Name)
 }
 
+type Shop struct {
+	TotalProfit int32
+	TotalLoss   int32
+}
+
+func (this *Shop) getGrossProfit(myStruct MyStruct) int32 {
+	myStruct.Name = "Ram"
+	this.TotalProfit = 100
+	return this.TotalProfit - this.TotalLoss
+}
+
 func main() {
-	var tempStr = new(MyStruct)
+	var tempStr MyStruct
 	tempStr.Name = "Gopal"
+
+	var coldStore = Shop{300, 300}
+	fmt.Println(coldStore.getGrossProfit(tempStr))
+	fmt.Println(coldStore.TotalProfit)
 	tempStr.fnInsideStruct()
+
 }
